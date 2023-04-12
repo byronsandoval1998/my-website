92eb07eebcdfc5c1e8aeda5bf9dfcd9ed5c65b17
Byron Sandoval <byron.sandoval1998@gmail.com>
2023-04-12 12:25:46 -0400

    Changed blog tab

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,11 @@ import Head from 'next/head';
 import path from 'path';
 import fs from 'fs';
 import matter from 'gray-matter';
+import Link from 'next/link';
 
 import About from "./About";
 import Start from "./Start";
 import Projects from "./Projects/Projects";
-import BlogElement from "./BlogElement";
 import propTypes from "prop-types";
 
 import { Tabs } from '../components/Navigation/Tabs';
@@ -32,7 +32,22 @@ export default function Home({ posts }) {
             <Projects />
           </div>
           <div label="Blog">
-            <BlogElement posts={posts} />
+          <div className="mx-auto m-0 max-w-2xl px-4 py-10">
+      <h1 className="text-3xl text-amber-50 font-sans">Posts</h1>
+      <ul className="mb-8 m-0 p-0">
+        {posts.map(post => (
+   
+          <li key={post.slug}>
+            <Link className="grid py-8 h-8" href={`/blog/${post.slug}`} target="_blank" rel="noreferrer">
+              <span className="font-thin font-sans text-amber-50 text-2xl cursor-pointer">{post.data.title}</span>
+            </Link>
+            <small className="text-gray-100">{post.data.createdAt}</small>
+            <p className="text-gray-300 text-md font-sans">{post.data.description}</p>
+          </li>
+
+        ))}
+      </ul>
+    </div>
           </div>
         </Tabs>
         
